edf8572ae1a95c849a6c90ec68938bc97144c33d
Edvan Reis <91618959+reisedvan@users.noreply.github.com>
2022-09-08 16:52:23 -0300

    Inserção de mensagem

--- ExemploAulas/src/exemploaulas/ExemploAulas.java
+++ ExemploAulas/src/exemploaulas/ExemploAulas.java
@@ -6,6 +6,7 @@ public class ExemploAulas {
    
     public static void main(String[] args) {
         //EXEMPLO PRA MOSTRAR COMO FUNCIONA
+       System.out.println("Teste de save em outro local");
     }
     
 }
