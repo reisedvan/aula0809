c0d953aee60be667285f015df3f1f279911e5e98
MKillua <MKillua@DESKTOP-KOG2PG9>
2022-09-08 16:56:50 -0300

    Alteração para inserção de soma

--- ExemploAulas/src/exemploaulas/ExemploAulas.java
+++ ExemploAulas/src/exemploaulas/ExemploAulas.java
@@ -5,8 +5,11 @@ public class ExemploAulas {
 
    
     public static void main(String[] args) {
-        //EXEMPLO PRA MOSTRAR COMO FUNCIONA
+        //EXEMPLO PRA MOSTRAR COMO FUNCIONA  Exemplo de modificação.
        System.out.println("Teste de save em outro local");
+       int x = 10;
+       int y = 20;
+        System.out.println("Soma = "+ (x+y));
     }
     
 }
