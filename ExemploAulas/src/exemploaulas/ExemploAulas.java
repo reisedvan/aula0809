c65051d732a391ff9bb66a6f94064e361e3d29cb
MKillua <MKillua@DESKTOP-KOG2PG9>
2022-09-09 14:30:11 -0300

    Implementação da função 'mostrarCPF'

--- ExemploAulas/src/exemploaulas/ExemploAulas.java
+++ ExemploAulas/src/exemploaulas/ExemploAulas.java
@@ -3,13 +3,18 @@ package exemploaulas;
 
 public class ExemploAulas {
 
-   
+    
+    private static void mostrarCPF(Pessoa pessoa){
+        System.out.println(pessoa.getCpf());
+    }
+    
     public static void main(String[] args) {
-        //EXEMPLO PRA MOSTRAR COMO FUNCIONA  Exemplo de modificação.
-       System.out.println("Teste de save em outro local");
-       int x = 10;
-       int y = 20;
-        System.out.println("Soma = "+ (x+y));
+       //EXEMPLO PRA MOSTRAR COMO FUNCIONA  Exemplo de modificação.
+       Pessoa x = new Pessoa("Edvan", "000.000.000-00", 22, 1.89);
+        mostrarCPF(x);
+        mostrarCPF(x);
+        mostrarCPF(x);
+       
     }
     
 }
